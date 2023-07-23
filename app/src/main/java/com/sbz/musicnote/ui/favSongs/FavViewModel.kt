b46712a40a360dbf529d0960c4966d8c5b9722a4
SHabaj-dev <shabajansari843@gmail.com>
2023-07-24 00:41:21 +0530

    Updated Fav Fragment

--- app/src/main/java/com/sbz/musicnote/ui/favSongs/FavViewModel.kt
+++ app/src/main/java/com/sbz/musicnote/ui/favSongs/FavViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class FavViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is Fav Fragment"
     }
     val text: LiveData<String> = _text
 }
